fe674ab293f0821d3a0cb2a31be63f79a7a2e7db
Ana Fernandes <49564843+fernandesanaf@users.noreply.github.com>
2022-03-19 19:24:21 -0300

    ultima não ta feita direito

--- listadeexercicios1.c
+++ listadeexercicios1.c
@@ -1,22 +1,98 @@
 /*
 INSTITUTO FEDERAL DO MATO GROSSO DO SUL
 Tecnologia em Análise e Desenvolvimento de Sistemas
-Ana Flávia de Brito Fernandes    |    Matrícula: 2021206476004-4 
+Ana Flávia de Brito Fernandes    |    RA:
 ana.fernandes8@estudante.ifms.edu.br
-DATA: 21/09/2021
-MATÉRIA: Construção de Algoritmos
-1 - faça um algoritmo que leia um numero inteiro diferente 
-de zero e diga se este é positivo ou negativo
+DATA: 18/03/2022
 */
+// #include <stdio.h>
+
+// int main()
+// {
+//     int num = 1;
+//     while(num<=10){
+//         printf("%d", num);
+//         num++;
+//     }
+
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int num = 100;
+//     while(num>=80){
+//         printf(" %d", num);
+//         num--;
+//     }
+
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int i, num = 30;
+//     while(num<=180){
+//         printf(" %d", num);
+//         num = num + 10;
+//     }
+
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int num = 1;
+//     while(num<=10){
+//         printf(" %d a, %d b, %d c", num, num, num);
+//         num++;
+//     }
+
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int numero, contador = 0;
+//     numero = 1;
+//     while (numero <= 311)
+//     {
+
+//         if (numero % 2 == 0)
+//         {
+//             contador++;
+//         }
+//         else
+//         {
+//             printf("\n %d \n", numero);
+//         }
+//         numero++;
+//     }
+// }
+
+// esse ainda não esta pronto
 #include <stdio.h>
 
-int main(){
-    int numero;
+int main()
+{
+    int numero, contador = 0;
+    numero = 1;
+    while (numero <= 403)
+    {
+        printf(" %d ", numero);
+        if (numero % 4 == 0)
+            printf("x");
 
-    printf("digite o numero: ");
-    scanf("%d", &numero);
-    if(numero >0)
-        printf("numero positivo");
-    else
-        printf("numero negativo"); 
+        contador++;
+
+        numero++;
+    }
 }
