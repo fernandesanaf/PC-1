548ef552662d0bb715d89260d75c3d54c464bd2d
Ana Fernandes <49564843+fernandesanaf@users.noreply.github.com>
2022-04-05 23:08:17 -0300

    vscode funcionando uhul

--- exercico1.c
+++ exercico1.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 void um()
 {
     int idade[50];
@@ -17,3 +18,74 @@ void um()
     float vazao_litros[5];
     float vol_pressao[20];
 }
+
+/*dois() Faça um programa que declare um vetor de cinco números inteiros. O
+usuário deve informar, um a um, todos os valores a serem armazenados no
+vetor. Ao final seu programa deverá imprimir todos os elementos do vetor.*/
+
+void dois(){
+    int num[5], i;
+    
+   
+    for(i=0;i<5;i++){
+        printf("digite o %dº numero: ", i+1);
+        scanf("%d", &num[i]);
+    }
+    
+    for(i=0;i<5;i++){
+        printf("o %dº numero digitado e: %d\n", i+1, num[i]);
+    }
+}
+
+/*tres() Faça um programa que declare um vetor de cinco números inteiros.
+Inicialize todas as posições do vetor com o valor zero e imprima o vetor.*/
+
+void tres(){
+    int num[5], i=0;
+
+    for(i=0;i<5;i++){
+        num[i]=0;
+    }
+    
+    for(i=0;i<5;i++){
+        printf("o %dº numero digitado e: %d\n", i+1, num[i]);
+    }
+}
+
+/*quatro() Faça um programa que declare um vetor de vinte números inteiros.
+O programa deverá atribuir em cada posição um número aleatório entre 1 e
+10. Ao final seu programa deverá imprimir todos os elementos do vetor, com
+suas respectivas posições.
+OBS: para gerar números aleatórios utilize a função rand() da biblioteca
+stdlib.h*/
+
+void quatro(){
+    int num[20], i;
+
+    for(i=0; i<10; i++){
+        printf("digite o numero: ");
+        scanf("%d", &num[i]);
+    }
+    for(i=0; i<10; i++){
+        num[rand()];
+    }
+}
+
+/*cinco() Faça um programa que declare um vetor de vinte números inteiros.
+O programa deverá atribuir em cada posição um número aleatório entre 0 e
+150. Ao final seu programa deverá imprimir todos os elementos do vetor.*/
+
+/*seis() Faça um programa que declare um vetor de vinte números inteiros. O
+programa deverá atribuir em cada posição um número aleatório entre 10 e 20.
+Ao final seu programa deverá imprimir todos os elementos do vetor, com suas
+respectivas posições.*/
+
+/*sete() Faça um programa que declare um vetor de 100 posições e preencha-o
+com números sequenciais de 1 até 100. Imprima o vetor.*/
+
+/*oito() Faça um programa que declare um vetor de 10 posições e preencha-o
+com números sequenciais decrescentes, iniciando em 99. Imprima o vetor.*/
+
+/*main() O usuário informa um número e executa-se o procedimento referente
+àquele número.*/
+
