24e09870e38a51e07ced5816d4e8eea14e83f39f
Ana Fernandes <49564843+fernandesanaf@users.noreply.github.com>
2022-05-02 23:06:37 -0300

    registro

---
+++ registro.c
@@ -0,0 +1,36 @@
+// Na linguagem C, defina um tipo registro que representa uma Hora e que
+//  contenha campos para hora, minuto e segundo. Declare um vetor de seis
+//  Horas. Armazene nas quatro primeiras posi¸c˜oes do vetor Horas
+//  informadas pelo usu´ario. Apenas Horas v´alidas podem ser aceitas.
+//  Armazene na quinta posi¸c˜ao do vetor a soma das quatro Horas (podendo
+//  exceder 24 horas). Na ´ultima posi¸c˜ao armazene a menor Hora cadastrada.
+//  Ao final apresente adequadamente todas as informa¸c˜oes do vetor.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Horas
+{
+    int hora;
+    int minuto;
+    int segundo;
+};
+
+int main()
+{
+    struct Horas a[6], i;
+
+    for (i == 0; i < 6; i++)
+    {
+        printf(" hora: ");
+        scanf("%d", &a.hora);
+
+        printf(" minuto: ");
+        scanf("%d", &a.minuto);
+
+        printf(" segundo: ");
+        scanf("%d", &a.minuto);
+    }
+
+    return 0;
+}
